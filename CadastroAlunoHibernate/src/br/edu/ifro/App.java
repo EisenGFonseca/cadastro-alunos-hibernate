9e5f64147b8679be5de5b716e3840300c8f6dc81
Dr <Dr@10.50.30.108>
2018-04-05 14:52:35 -0400

    Hibernate

---
+++ CadastroAlunoHibernate/src/br/edu/ifro/App.java
@@ -0,0 +1,38 @@
+package br.edu.ifro;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class App extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("aula");
+        EntityManager em = emf.createEntityManager();
+        
+        Aluno aluno1 = new Aluno();
+        aluno1.setNome("Fulano");
+        
+        em.getTransaction().begin();
+        em.persist(aluno1);
+        em.getTransaction().commit();
+        
+        Parent root = FXMLLoader.load(getClass().getResource("TelaPrincipal.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
